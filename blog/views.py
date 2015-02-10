db36cd77a6997cefb3c85a9000e36c6138767ccd
eriksdav <david.eriksson@ehalsomyndigheten.se>
2015-02-10 16:28:09 +0100

    start på två sidor

--- blog/views.py
+++ blog/views.py
@@ -1,35 +1,38 @@
 from django.shortcuts import render
-from blog.models import posts
 from blog.newmodel import posts
+import os
 
 def home(request):
-    #entries = posts.objects.all()[:10]
-    entries = []
-    for i in range(3):
-        entries.append(posts)
-    return render(request, 'index.html', {'posts' : entries})
+    if 'inputtext' in request.POST and request.POST['inputtext']:
+        q = request.POST['inputtext']
+        dirs = [os.path.join(dirpath, f)
+            for dirpath, dirnames, files in os.walk(q)
+            for f in dirnames if f.endswith('.git')]
+
+    return render(request, 'second.html', {'dirs':dirs})
+
 
 def about(request):
-    #entries = posts.objects.all()[:10]
     entries = []
     for i in range(2):
         entries.append(posts)
-    return render(request, 'index.html', {'posts' : entries})
+    return render(request, 'index.html', {'posts': entries})
+
 
 def contact(request):
-    #entries = posts.objects.all()[:10]
     entries = []
     for i in range(4):
         entries.append(posts)
-    return render(request, 'index.html', {'posts' : entries})
+    return render(request, 'index.html', {'posts': entries})
+
 
 def input(request):
     entries = []
     if 'inputtext' in request.POST and request.POST['inputtext']:
         q = request.POST['inputtext']
-        entries = [{'author' : q, 'title' : 'other title', 'bodytext' : 'other bodytext', 'timestamp' : 'other timestamp'}]
+        entries = [{'author': q, 'title': 'other title', 'bodytext': 'other bodytext', 'timestamp': 'other timestamp'}]
     if 'inputtext' in request.GET and request.GET['inputtext']:
         q = request.GET['inputtext']
-        entries = [{'author' : q, 'title' : 'other title', 'bodytext' : 'other bodytext', 'timestamp' : 'other timestamp'}]
+        entries = [{'author': q, 'title': 'other title', 'bodytext': 'other bodytext', 'timestamp': 'other timestamp'}]
 
-    return render(request, 'index.html', {'posts' : entries})
+    return render(request, 'index.html', {'posts': entries})
