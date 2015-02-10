37169ef0f705530112a1cffd4693781c49510259
david eriksson <davideriksson@swedenmail.com>
2015-02-10 21:54:13 +0100

    fixed

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,9 @@ from django.shortcuts import render
 from blog.newmodel import posts
 import os
 
-def home(request):
-    if 'inputtext' in request.POST and request.POST['inputtext']:
-        q = request.POST['inputtext']
-        dirs = [os.path.join(dirpath, f)
-            for dirpath, dirnames, files in os.walk(q)
-            for f in dirnames if f.endswith('.git')]
 
-    return render(request, 'second.html', {'dirs':dirs})
+def home(request):
+    return render(request, 'second.html')
 
 
 def about(request):
@@ -27,12 +22,10 @@ def contact(request):
 
 
 def input(request):
-    entries = []
+    dirs = []
     if 'inputtext' in request.POST and request.POST['inputtext']:
         q = request.POST['inputtext']
-        entries = [{'author': q, 'title': 'other title', 'bodytext': 'other bodytext', 'timestamp': 'other timestamp'}]
-    if 'inputtext' in request.GET and request.GET['inputtext']:
-        q = request.GET['inputtext']
-        entries = [{'author': q, 'title': 'other title', 'bodytext': 'other bodytext', 'timestamp': 'other timestamp'}]
-
-    return render(request, 'index.html', {'posts': entries})
+        dirs = [os.path.join(dirpath, f)
+            for dirpath, dirnames, files in os.walk(q)
+            for f in dirnames if f.endswith('.git')]
+    return render(request, 'index.html', {'dirs': dirs})
