33b37cc063456735dc6279c7df1830c2114048f1
david eriksson <davideriksson@swedenmail.com>
2015-03-29 22:41:30 +0200

    test for input

--- blog/tests.py
+++ blog/tests.py
@@ -112,6 +112,8 @@ class StartPageTests(TestCase):
 
 class SecondPageTests(TestCase):
     def setUp(self):
+        self.base_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.git')
+
         user_info = UserInfo.create(username='user')
         user_info.save()
         self.user_info = user_info
@@ -155,6 +157,26 @@ class SecondPageTests(TestCase):
             'workspace_name': 'workspace_name'})
         self.assertEqual(response.status_code, 200)
         self.assertFalse(Workspace.objects.filter(workspace='workspace_name'))
+
+    def test_remove_workspace_without_username(self):
+        response = self.client.post('/remove/workspace/',
+            {'workspace_name': 'workspace_name'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page(self):
+        response = self.client.get('/about')
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_page(self):
+        response = self.client.get('/contact')
+        self.assertEqual(response.status_code, 200)
+
+    def test_input(self):
+        Workspace.create(user_info=self.user_info, workspace='main').save()
+        workspace = self.base_dir.replace('djangoblog/.git', '')
+        response = self.client.post('/input/', 
+            {"input_text": workspace, "username": "user"})
+        self.assertEqual(response.status_code, 200)
 
 
 class GitManagerTests(TestCase):
