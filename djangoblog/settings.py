910ec38fd4bb9b48a62e1f91eaf9598a35cf900a
eriksdav <david.eriksson@ehalsomyndigheten.se>
2015-04-01 10:01:42 +0200

    small fix

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -86,9 +86,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'blog/templates'),
