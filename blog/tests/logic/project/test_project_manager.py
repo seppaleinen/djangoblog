b3ec3e2f49287feb2976bb6e5d9bc7204ad7c3d8
david eriksson <davideriksson@swedenmail.com>
2015-04-20 22:57:34 +0200

    Tests

--- blog/tests/logic/project/test_project_manager.py
+++ blog/tests/logic/project/test_project_manager.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from blog.logic.project.project_manager import ProjectManager
 from blog import views
 import os
+from blog.tests.resources import maven
 
 
 class ProjectManagerTest(TestCase):
@@ -16,6 +17,21 @@ class ProjectManagerTest(TestCase):
 		result = self.project_manager.search_dir_for_projects(path=self.base_dir)
 		self.assertEquals(1, len(result))
 
-	def test_define_project_type(self):
+	def test_define_project_type_django(self):
 		result = self.project_manager.define_project_type(path=self.base_dir)
 		self.assertEquals(result, 'django')
+
+	def test_define_project_type_maven(self):
+		maven_path = os.path.join(os.path.dirname(os.path.dirname(maven.__file__)), 'maven')
+		result = self.project_manager.define_project_type(path=maven_path)
+		self.assertEquals(result, 'maven')
+
+	def test_define_project_type_gradle(self):
+		gradle_path = os.path.join(os.path.dirname(os.path.dirname(maven.__file__)), 'gradle')
+		result = self.project_manager.define_project_type(path=gradle_path)
+		self.assertEquals(result, 'gradle')
+
+	def test_define_project_type_pysetup(self):
+		pysetup_path = os.path.join(os.path.dirname(os.path.dirname(maven.__file__)), 'pysetup')
+		result = self.project_manager.define_project_type(path=pysetup_path)
+		self.assertEquals(result, 'pysetup')
