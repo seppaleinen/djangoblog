edd15f3dabd734a7be561b8585f76915c3477c74
david eriksson <davideriksson@swedenmail.com>
2015-04-29 21:43:23 +0200

    Splinter test

--- blog/tests/test_splinter.py
+++ blog/tests/test_splinter.py
@@ -1,16 +1,10 @@
 from splinter import Browser
-from django.test import TestCase
+from django.test import LiveServerTestCase
 
-class TestClass(TestCase):
+class TestClass(LiveServerTestCase):
     @classmethod
     def setUpClass(cls):
-        #try:
-        #    cls.selenium = webdriver.Firefox()
-        #    cls.selenium.maximize_window()
-        #    super(TestClass, cls).setUpClass()
-        #except Exception:
-        #    raise SkipTest("Firefox not installed on system")
-        cls.browser = Browser('zope.testbrowser', ignore_robots=True)
+        cls.browser = Browser('zope.testbrowser')
         super(TestClass, cls).setUpClass()
 
     @classmethod
@@ -19,15 +13,12 @@ class TestClass(TestCase):
         super(TestClass, cls).tearDownClass()
 
     def test_google(self):
-        # Visit URL
-        url = "http://splinter.readthedocs.org/en/latest/index.html"
+        url = "http://localhost:8081"
         self.browser.visit(url)
-        #self.browser.fill('q', 'splinter - python acceptance testing for web applications')
-        # Find and click the 'search' button
-        #button = self.browser.find_by_name('btnG')
-        # Interact with elements
+        self.assertTrue('<form action="/username/" method="post">' in self.browser.html)
+        #print(self.browser.html)
+        #self.browser.fill('username', 'testuser')
+        #button = self.browser.find_by_value('OK')
         #button.click()
-        if self.browser.is_text_present('Splinter is an open source'):
-            print "Yes, the official website was found!"
-        else:
-            print "No, it wasn't found... We need to improve our SEO techniques"
+        #result = self.browser.is_text_present('Directory to workspace')
+        #self.assertTrue(result)
