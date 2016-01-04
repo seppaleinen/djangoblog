fe3c3769759d3385415be74aa734abf12cb57812
David Eriksson <davideriksson@swedenmail.com>
2016-01-04 23:35:25 +0100

    Fixed deprecations

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -52,6 +52,30 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'blog/templates'),
+            os.path.join(BASE_DIR, 'tdd/templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 ROOT_URLCONF = 'djangoblog.urls'
 
 WSGI_APPLICATION = 'djangoblog.wsgi.application'
--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -4,26 +4,17 @@ from tdd import views
 import tdd.urls
 from django.conf import settings
 from django.conf.urls.static import static
+from blog.views import home, about, contact, input_view, add_workspace, remove_workspace, username
 
-urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'blog.views.home', name='home'),
-
-    url(r'^about$', 'blog.views.about', name='about'),
-
-    url(r'^contact$', 'blog.views.contact', name='contact'),
-
-    url(r'^input/', 'blog.views.input_view', name='input_view'),
-
-    url(r'^add/workspace/', 'blog.views.add_workspace', name='add_workspace'),
-
-    url(r'^remove/workspace/', 'blog.views.remove_workspace', name='remove_workspace'),
-
-    url(r'^username/', 'blog.views.username', name='username'),
-
+urlpatterns = [
+    url(r'^$', home, name='home'),
+    url(r'^about$', about, name='about'),
+    url(r'^contact$', contact, name='contact'),
+    url(r'^input/', input_view, name='input_view'),
+    url(r'^add/workspace/', add_workspace, name='add_workspace'),
+    url(r'^remove/workspace/', remove_workspace, name='remove_workspace'),
+    url(r'^username/', username, name='username'),
     url(r'^tdd/', views.HomeView.as_view(), name='home_view'),
-
     url(r'^admin/', include(admin.site.urls)),
-
     url(r'^', include(tdd.urls)),
-) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
