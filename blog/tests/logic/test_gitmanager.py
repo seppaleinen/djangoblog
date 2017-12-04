f0357bb059d77e11305c2d29ecee41801b83dfe1
David Eriksson <davideriksson@swedenmail.com>
2017-12-04 19:30:16 +0100

    Fixed tests

--- blog/tests/logic/test_gitmanager.py
+++ blog/tests/logic/test_gitmanager.py
@@ -7,34 +7,34 @@ from blog import views
 import os
 
 class GitManagerTests(TestCase):
-    def setUp(self):
-        self.base_dir = os.path.join(os.path.dirname(os.path.dirname(views.__file__)), '.git')
+	def setUp(self):
+		self.base_dir = os.path.join(os.path.dirname(os.path.dirname(views.__file__)), '.git')
 
-    def test_get_all_branches(self):
-        result = get_all_branches(directory_name=self.base_dir)
-        self.assertTrue('master' in line for line in result)
+	def test_get_all_branches(self):
+		result = get_all_branches(directory_name=self.base_dir)
+		self.assertTrue('master' in line for line in result)
 
-    def test_git_check_updates_all_branches(self):
-        result = git_check_updates_all_branches(directory_name=self.base_dir)
-        self.assertTrue('(up to date)' in line for line in result)
+	def test_git_check_updates_all_branches(self):
+		result = git_check_updates_all_branches(directory_name=self.base_dir)
+		self.assertTrue('(up to date)' in line for line in result)
 
-    def test_git_checkout_to_branch(self):
-    	results = get_all_branches(directory_name=self.base_dir)
-    	current_branch = None
-    	for result in results:
-    		if '*' in result:
-    			current_branch = result.replace('* ', '').replace('\n','')
-    	self.assertIsNotNone(current_branch)
-    	checkout_result = checkout_to_branch(directory_name=self.base_dir, branch_name=current_branch)
-    	self.assertIsNotNone(checkout_result)
+	def test_git_checkout_to_branch(self):
+		results = get_all_branches(directory_name=self.base_dir)
+		current_branch = None
+		for result in results:
+			if '*' in result:
+				current_branch = result.replace('* ', '').replace('\n','')
+		self.assertIsNotNone(current_branch)
+		checkout_result = checkout_to_branch(directory_name=self.base_dir, branch_name=current_branch)
+		self.assertIsNotNone(checkout_result)
 
-    def test_git_pull(self):
-    	results = get_all_branches(directory_name=self.base_dir)
-    	current_branch = None
-    	for result in results:
-    		if '*' in result:
-    			current_branch = result.replace('* ', '').replace('\n', '')
+	def test_git_pull(self):
+		results = get_all_branches(directory_name=self.base_dir)
+		current_branch = None
+		for result in results:
+			if '*' in result:
+				current_branch = result.replace('* ', '').replace('\n', '')
 
-    	self.assertIsNotNone(current_branch)
-    	git_pull_result = git_pull(directory_name=self.base_dir, branch_name=current_branch)
-    	self.assertIsNotNone(git_pull_result)
+		self.assertIsNotNone(current_branch)
+		git_pull_result = git_pull(directory_name=self.base_dir, branch_name=current_branch)
+		self.assertIsNotNone(git_pull_result)
